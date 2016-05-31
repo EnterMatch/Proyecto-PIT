03aa1bfabe8abd0a1a5e02d63f7041cce54440a1
EnterMatch <y50001@hotmail.com>
2016-05-31 00:06:56 -0500

    Test modificado

--- proyectoPIT/src/test/ClienteServiceTest.java
+++ proyectoPIT/src/test/ClienteServiceTest.java
@@ -16,42 +16,23 @@ public class ClienteServiceTest{
 	int result = 0;
 	@Before
 	public void setUp() throws Exception {
-		result = 0;
+		
 	}
 
 	@Test
-	public void createTest() {
-		result = clienteService.create(new Cliente(0, "Nombre", "Email"));
+	public void crudTest() {
+		Cliente cliente = new Cliente(0, "Nombre", "Email");
+		result = clienteService.create(cliente);
 		assertEquals(1, result);
-	}
-
-	@Test
-	public void updateTest() {
-		List<Cliente> clientes = clienteService.read();
-		Cliente cliente = clientes.get(clientes.size()-1);
 		result = clienteService.update(new Cliente(cliente.getIdCliente(), "Nombre", "Email"));
 		assertEquals(1, result);
-	}
-	
-	@Test
-	public void readTest() {
 		assertNotNull(clienteService.read());
-	}
-
-	@Test
-	public void obtainTest() {
-		List<Cliente> clientes = clienteService.read();
-		Cliente cliente = clientes.get(clientes.size()-1);
 		assertNotNull(clienteService.obtain(cliente.getIdCliente()));
-	}
-
-	@Test
-	public void deleteTest() {
-		List<Cliente> clientes = clienteService.read();
-		Cliente cliente = clientes.get(clientes.size()-1);
 		result = clienteService.delete(cliente.getIdCliente());
+		assertEquals(1, result);
 	}
 
+	
 
 	
 }
